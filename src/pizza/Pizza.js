fcb4fb4e55cec73e6ac06f70c70a8bb668a0668d
Luiz Antonio Massa de Pontes <lamp@cesar-ldap.intranet>
2023-08-02 11:45:04 -0300

    finish the pizza menu

--- src/pizza/Pizza.js
+++ src/pizza/Pizza.js
@@ -9,49 +9,64 @@ function Header() {
   );
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{!pizzaObj.soldOut ? pizzaObj.price : "Sold Out"}</span>
       </div>
     </li>
   );
 }
 
 function Menu() {
+  const numPizzas = pizzaData.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {!numPizzas.length ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
   const closeHour = 22;
-
   const isOpen = hour >= openHour && hour <= closeHour;
-  let pizzariaState = "";
+  return isOpen ? (
+    <Order closeHour={closeHour} />
+  ) : (
+    <div className="footer">
+      <p>Sorry, we're closed.</p>
+    </div>
+  );
+}
 
-  if (isOpen) {
-    pizzariaState = "We're currently open!";
-  } else {
-    pizzariaState = "Sorry, we're closed";
-  }
-
+function Order({ closeHour }) {
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} - {pizzariaState}
+      <div className="order">
+        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+        <button className="btn">Order</button>
+      </div>
     </footer>
   );
 }
